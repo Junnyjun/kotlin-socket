1b8b6584c207571d5fc4941946ad3ce1a15613d5
Junnyjun <chbe5082@naver.com>
2022-10-09 19:09:14 +0900

    inti

---
+++ src/test/kotlin/git/junny/kotlinsocket/KotlinSocketApplicationTests.kt
@@ -0,0 +1,13 @@
+package git.junny.kotlinsocket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSocketApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
